75bc3fdb27f68ce2d5700231f449fb702e8ea802
halogoo <halogoo@10.162.3.14>
2019-10-05 14:42:20 -0700

    Second Version of Assignment 2.

--- Assignment2/src/cse360assign2/AddingMachine.java
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -6,31 +6,64 @@
  */
 package cse360assign2;
 
-public class AddingMachine {
+public class AddingMachine 
+{
 
 	private int total;
+	private String history;
 	
-	public AddingMachine () {
+	/**
+	 * Constructor for AddingMachine
+	 */
+	public AddingMachine () 
+	{
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
-	public int getTotal () {
-		return 0;
+	/**
+	 * 
+	 * @return total number in adding machine
+	 */
+	public int getTotal () 
+	{
+		return total;
 	}
 	
-	public void add (int value) {
-		
+	/**
+	 * 
+	 * @param value is added to total
+	 */
+	public void add (int value) 
+	{
+		total += value;
+		history += " + " + value;
 	}
 	
-	public void subtract (int value) {
-		
+	/**
+	 * 
+	 * @param value is subtracted from total
+	 */
+	public void subtract (int value) 
+	{
+		total -= value;
+		history += " - " + value;
 	}
 		
-	public String toString () {
-		return "";
+	/**
+	 * @return history of transactions is returned
+	 */
+	public String toString () 
+	{
+		return history;
 	}
 
-	public void clear() {
-	
+	/**
+	 * Clears the AddingMachine of all calculation history
+	 */
+	public void clear () 
+	{
+		total = 0;
+		history = "0";
 	}
 }
