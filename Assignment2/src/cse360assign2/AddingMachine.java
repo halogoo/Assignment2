29f48419378270dc59f2a57aaa383b5a16998555
halogoo <halogoo@10.162.3.14>
2019-10-05 14:10:06 -0700

    Initial commit of Assignment2.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,36 @@
+/**
+ * Name: Ryan Hill
+ * Class ID: 70642
+ * Assignment: 2
+ * Class Description: Adds and Subtracts Values from a total number
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
